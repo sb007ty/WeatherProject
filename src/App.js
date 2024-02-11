b1ff1f3a0290e4ac5defeab7be138df9a8d59ef0
Spandan <spandanbanerjee28@gmail.com>
2024-02-11 22:14:33 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Card from "./components/Card";
 import { useState } from "react";
 import Weather from "./components/Weather";
+import axios from "axios";
 
 function App() {
   const [searchText, setSearchText] = useState("");
@@ -11,21 +12,22 @@ function App() {
   console.log("rerender", search);
   async function fetchDetails(city) {
     try {
-      const response = await fetch(
+      // const response = await fetch(
+      //   `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+      // );
+      const response = await axios.get(
         `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=fe4feefa8543e06d4f3c66d92c61b69c`
       );
-      if (response.ok) {
-        console.log(response, " respon**");
-        const data = await response.json();
-        console.log(typeof data, "abc", data);
-        const { lat, lon } = data[0];
-        const weatherResp = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
-        );
-        const weatherData = await weatherResp.json();
-        console.log(weatherData, " wea");
-        setWeatherData(weatherData);
-      }
+      console.log(response, " respon**ARR");
+      const data = response.data;
+      console.log(typeof data, "abc", data);
+      const { lat, lon } = data[0];
+      const weatherResp = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+      );
+      const weatherData = await weatherResp.json();
+      console.log(weatherData, " wea");
+      setWeatherData(weatherData);
     } catch (error) {
       console.log(error);
     }
